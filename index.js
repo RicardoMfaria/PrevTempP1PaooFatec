37b70c5f0c32148189a8b2e11c394bf502bb36fd
Ricardo Marques <contato.ricardotr@gmail.com>
2025-09-30 19:51:51 -0300

    feat(p1): obtem previsao com async await e try catch

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 require('dotenv').config()
 const axios = require('axios')
 
-const PROTOCOL = 'https'
-const BASE_URL = 'api.openweathermap.org/geo/1.0/direct'
-const APPID = process.env.OPENWEATHER_API_KEY
-const Q = 'são paulo'
-const URL = `${PROTOCOL}://${BASE_URL}?q=${Q}&appid=${APPID}`
+const CHAVE = process.env.OPENWEATHER_API_KEY
 
+const obterPrevisao = async (lat, lon) => {
+  try {
+    const urlPrevisao = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${CHAVE}&units=metric&lang=pt_br`;
+    const resposta = await axios.get(urlPrevisao);
+    const { feels_like } = resposta.data.main;
+    const { description } = resposta.data.weather[0];
 
-const promiseResultante = axios.get(URL)
-promiseResultante.then((coordenada) =>{
-    const {lat, lon} = coordenada.data[0] 
+    console.log("\nPrevisão do Tempo");
+    console.log(`Sensação térmica: ${feels_like}°C`);
+    console.log(`Descrição: ${description}`);
+  } catch (erro) {
+    console.log(`Erro ao obter a previsão do tempo:${erro}`);
+  }
+}
+
+const CIDADE = "são paulo"
+const urlCoordenadas = `https://api.openweathermap.org/geo/1.0/direct?q=${CIDADE}&appid=${CHAVE}`;
+
+axios.get(urlCoordenadas)
+  .then((coordenada) => {
+    const { lat, lon } = coordenada.data[0]
+    console.log("Coordenadas");
     console.log(`Latitude: ${lat}`);
     console.log(`Longitude: ${lon}`);
-})
-.catch((erro) => {
-    console.log(`Erro ao buscar as coordenadas ${erro}`)
-})
-
+    obterPrevisao(lat, lon);
+  })
+  .catch((erro) => {
+    console.log(`Erro ao buscar as coordenadas: ${erro}`)
+  })
